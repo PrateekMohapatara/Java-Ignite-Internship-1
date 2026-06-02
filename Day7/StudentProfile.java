9a85863fca5f40db6090028b971523f69947351f
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-02 21:00:47 +0530

    Update StudentProfile.java

--- Day7/StudentProfile.java
+++ Day7/StudentProfile.java
@@ -17,3 +17,40 @@ Marks should not be accessed directly.
 Write your code below
 ----------------------------------------
 */
+package day7_prog;
+
+class Studentm {
+
+    String name;
+    int age;
+    private int marks;
+
+    Studentm(String name, int age, int marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    void showDetails() {
+        System.out.println("Name  : " + name);
+        System.out.println("Age   : " + age);
+        System.out.println("Marks : " + getMarks());
+        System.out.println();
+    }
+}
+
+public class student {
+
+    public static void main(String[] args) {
+
+        Studentm s1 = new Studentm("Prateek", 19, 88);
+        Studentm s2 = new Studentm("Rahul", 20, 92);
+
+        s1.showDetails();
+        s2.showDetails();
+    }
+}
