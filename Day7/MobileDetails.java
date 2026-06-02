9c0c1be707a3d3679da93bdaf26282f6f1af13bb
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-02 20:57:39 +0530

    Update MobileDetails.java

--- Day7/MobileDetails.java
+++ Day7/MobileDetails.java
@@ -16,3 +16,36 @@ Twist:
 
 Price must be private and only shown via method.
   */
+package day7_prog;
+class MobileDetails {
+    String brand;
+    int storage;
+    private int price;
+
+    MobileDetails(String brand,int price,int storage){
+        this.brand=brand;
+        this.price=price;
+        this.storage=storage;
+    }
+
+    void showDetails() {
+        System.out.println("Brand : " + brand);
+        System.out.println("Price : " + price);
+        System.out.println("Storage : " + storage + " GB");
+    }
+}
+
+public class Mobile {
+
+    public static void main(String[] args) {
+
+        MobileDetails m1 =
+            new MobileDetails("Samsung",25000,128);
+
+        MobileDetails m2 =
+            new MobileDetails("OnePlus",30000,256);
+
+        m1.showDetails();
+        m2.showDetails();
+    }
+}
